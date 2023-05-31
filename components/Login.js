41e2360ad8a12c594460db61fa03ed2bb9537453
Aremu Oluwaseyi Festus <oluwaremu1407@gmail.com>
2023-05-31 13:48:32 +0100

    Feat: Added Email, and Password checks

--- components/Login.js
+++ components/Login.js
@@ -1,5 +1,5 @@
 import React from "react";
-import IMG_8165 from "../assets/images/background.jpg";
+import IMG_8165 from "../assets/images/LoginBackground.jpg";
 import { Button } from "@rneui/themed";
 
 import {
@@ -9,42 +9,67 @@ import {
   View,
   TextInput,
   TouchableOpacity,
+  KeyboardAvoidingView,
+  Alert,
 } from "react-native";
 
 export function LoginScreen({ navigation }) {
+  const [email, setEmail] = React.useState("");
+
+  const handleEmailChange = (text) => {
+    setEmail(text);
+  };
+
+  const validateEmail = () => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+      Alert.alert("Invalid Email", "Please enter a valid email address.");
+    } else {
+      // Email is valid, perform further actions
+      // e.g., submit form, call API, etc.
+    }
+  };
   return (
-    <ImageBackground source={IMG_8165} style={styles.backgroundImage}>
-      <View style={styles.loginInfoContainer}>
-        <View style={styles.loginInfoContainer.inputContainer}>
-          <Text style={styles.loginInfoContainer.label}>Email</Text>
-          <TextInput
-            style={styles.loginInfoContainer.input}
-            placeholder="john.doe@email.com"
-          />
+    <KeyboardAvoidingView
+      style={{ flex: 1 }}
+      behavior={Platform.OS === "ios" ? "padding" : undefined}
+    >
+      <ImageBackground source={IMG_8165} style={styles.backgroundImage}>
+        <View style={styles.loginInfoContainer}>
+          <View style={styles.loginInfoContainer.inputContainer}>
+            <Text style={styles.loginInfoContainer.label}>Email</Text>
+            <TextInput
+              style={styles.loginInfoContainer.input}
+              placeholder="john.doe@email.com"
+              value={email}
+              onChangeText={handleEmailChange}
+              keyboardType="email-address"
+            />
+          </View>
+          <View style={styles.loginInfoContainer.inputContainer}>
+            <Text style={styles.loginInfoContainer.label}>Password</Text>
+            <TextInput
+              style={styles.loginInfoContainer.input}
+              placeholder="Password"
+              secureTextEntry={true}
+            />
+            <Button
+              style={styles.loginInfoContainer.loginButton}
+              title="LOG IN"
+              titleStyle={{ fontWeight: "bold", color: "white" }}
+              type="clear"
+              onPress={() => console.log("aye")}
+            />
+          </View>
+          <Text style={styles.loginInfoContainer.bottomText}>
+            Dont't have an account?{" "}
+            <TouchableOpacity onPress={() => navigation.navigate("Signup")}>
+              <Text style={styles.loginInfoContainer.signUpLink}>SIGN UP</Text>
+            </TouchableOpacity>
+          </Text>
         </View>
-        <View style={styles.loginInfoContainer.inputContainer}>
-          <Text style={styles.loginInfoContainer.label}>Password</Text>
-          <TextInput
-            style={styles.loginInfoContainer.input}
-            placeholder="Password"
-            secureTextEntry={true}
-          />
-          <Button
-            style={styles.loginInfoContainer.loginButton}
-            title="LOG IN"
-            titleStyle={{ fontWeight: "bold", color: "white" }}
-            type="clear"
-            onPress={() => console.log("aye")}
-          />
-        </View>
-        <Text style={styles.loginInfoContainer.bottomText}>
-          Dont't have an account?{" "}
-          <TouchableOpacity onPress={() => navigation.navigate("Signup")}>
-            <Text style={styles.loginInfoContainer.signUpLink}>SIGN UP</Text>
-          </TouchableOpacity>
-        </Text>
-      </View>
-    </ImageBackground>
+      </ImageBackground>
+    </KeyboardAvoidingView>
   );
 }
 
@@ -72,7 +97,7 @@ const styles = StyleSheet.create({
     },
     input: {
       borderWidth: 1,
-      borderColor: "gray",
+      borderColor: "#922268",
       padding: 10,
       borderRadius: 5,
       width: 250,
@@ -82,7 +107,7 @@ const styles = StyleSheet.create({
       width: 150,
       marginTop: 15,
       marginLeft: 50,
-      backgroundColor: "#d8439f",
+      backgroundColor: "#922268",
       borderColor: "white",
       borderRadius: 5,
     },
--- components/Signup.js
+++ components/Signup.js
@@ -1,12 +1,140 @@
 import React from "react";
-import { StyleSheet, ImageBackground, View } from "react-native";
-import IMG_8165 from "../assets/images/background.jpg";
+import {
+  StyleSheet,
+  ImageBackground,
+  View,
+  Text,
+  TextInput,
+  KeyboardAvoidingView,
+  Alert,
+} from "react-native";
+import IMG_8165 from "../assets/images/SignUpBackground.jpg";
+import { Button } from "@rneui/themed";
 
 export function SignUpScreen() {
+  const [value, setValue] = React.useState("");
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+
+  const handlePasswordChange = (text) => {
+    setPassword(text);
+  };
+
+  const handleTextChange = (text) => {
+    // Remove non-numeric characters using regular expression
+    const numericValue = text.replace(/[^0-9]/g, "");
+    setValue(numericValue);
+  };
+
+  const handleEmailChange = (text) => {
+    setEmail(text);
+  };
+
+  const validateEmail = () => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const passwordPattern =
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+
+    if (!emailRegex.test(email)) {
+      Alert.alert("Invalid Email", "Please enter a valid email address.");
+    }
+
+    if (!passwordPattern.test(password)) {
+      Alert.alert(
+        "Invalid Password",
+        "Password must contain at least 8 characters, including one lowercase letter, one uppercase letter, one digit, and one special character."
+      );
+    }
+  };
   return (
+    // <KeyboardAvoidingView
+    //   style={{ flex: 2 }}
+    //   behavior={Platform.OS === "ios" ? "padding" : undefined}
+    //   keyboardVerticalOffset={10}
+    // >
     <ImageBackground source={IMG_8165} style={styles.backgroundImage}>
-      <View></View>
+      <View style={styles.signupContainer}>
+        <Text style={styles.signupMessage}> SIGN UP</Text>
+        {/* <Text> Please kindly provide accurate information </Text> */}
+
+        <View style={styles.signupContainer.nameContainer}>
+          <View>
+            <Text style={styles.signupContainer.inputContainer.label}>
+              First Name
+            </Text>
+
+            <TextInput
+              style={styles.signupContainer.nameContainer.inputName}
+              placeholder="John"
+            />
+          </View>
+          <View>
+            <Text style={styles.signupContainer.inputContainer.label}>
+              Last Name
+            </Text>
+            <TextInput
+              style={styles.signupContainer.nameContainer.inputName}
+              placeholder="Doe"
+            />
+          </View>
+        </View>
+        <View style={styles.signupContainer.inputContainer}>
+          <Text style={styles.signupContainer.inputContainer.label}>Email</Text>
+          <TextInput
+            style={styles.signupContainer.input}
+            placeholder="example@email.com"
+            value={email}
+            onChangeText={handleEmailChange}
+            keyboardType="email-address"
+          />
+        </View>
+        <View style={styles.signupContainer.inputContainer}>
+          <Text style={styles.signupContainer.inputContainer.label}>
+            Phone Number
+          </Text>
+          <TextInput
+            style={styles.signupContainer.input}
+            placeholder="+234 7832479232"
+            value={value}
+            onChangeText={handleTextChange}
+            keyboardType="phone-pad"
+            maxLength={15}
+          />
+        </View>
+        <View style={styles.signupContainer.nameContainer}>
+          <View>
+            <Text style={styles.signupContainer.inputContainer.label}>
+              Password
+            </Text>
+            <TextInput
+              style={styles.signupContainer.nameContainer.inputName}
+              placeholder="Password"
+              secureTextEntry
+              value={password}
+              onChangeText={handlePasswordChange}
+            />
+          </View>
+          <View>
+            <Text style={styles.signupContainer.inputContainer.label}>
+              Confirm password
+            </Text>
+            <TextInput
+              style={styles.signupContainer.nameContainer.inputName}
+              placeholder="Confirm password"
+              secureTextEntry
+            />
+          </View>
+        </View>
+        <Button
+          style={styles.signupContainer.loginButton}
+          title="SIGN UP"
+          titleStyle={{ fontWeight: "bold", color: "white" }}
+          type="clear"
+          onPress={validateEmail}
+        />
+      </View>
     </ImageBackground>
+    // </KeyboardAvoidingView>
   );
 }
 
@@ -14,8 +142,67 @@ const styles = StyleSheet.create({
   backgroundImage: {
     marginTop: 45,
     flex: 1,
-    resizeMode: "cover",
-    width: "100%",
-    flexDirection: "column",
+    resizeMode: "contain",
+    width: 450,
+    height: 200,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  signupMessage: {
+    color: "black",
+    fontSize: 35,
+  },
+  signupContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    inputContainer: {
+      marginBottom: 10,
+    },
+    label: {
+      fontSize: 12,
+      fontWeight: "bold",
+      marginBottom: 10,
+    },
+    input: {
+      borderWidth: 1,
+      borderColor: "#922268",
+      padding: 10,
+      borderRadius: 5,
+      fontSize: 15,
+      width: 350,
+      backgroundColor: "white",
+      shadowColor: "black",
+      shadowOffset: { width: 0, height: 2 },
+      shadowOpacity: 0.1,
+      shadowRadius: 4,
+    },
+    loginButton: {
+      width: 150,
+      marginTop: 15,
+      backgroundColor: "#922268",
+      borderColor: "white",
+      borderRadius: 5,
+    },
+    nameContainer: {
+      flexDirection: "row",
+      justifyContent: "space-between",
+      alignItems: "center",
+      width: 350,
+      marginBottom: 10,
+      inputName: {
+        borderWidth: 1,
+        borderColor: "#922268",
+        padding: 10,
+        borderRadius: 5,
+        fontSize: 15,
+        width: 170,
+        backgroundColor: "white",
+        shadowColor: "black",
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.1,
+        shadowRadius: 4,
+      },
+    },
   },
 });
