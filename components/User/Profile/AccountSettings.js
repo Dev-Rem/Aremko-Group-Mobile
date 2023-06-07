d2ba16e48827f9bb1c7e6fbff1f4986fe28819f9
Aremu Oluwaseyi Festus <oluwaremu1407@gmail.com>
2023-06-07 17:19:51 +0100

    Add account setting options

---
+++ components/User/Profile/AccountSettings.js
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import { ScrollView, TouchableOpacity } from "react-native";
+import { ListItem } from "@rneui/themed";
+import { Icon } from "@rneui/themed";
+
+const AccountSettings = () => {
+  return (
+    <ScrollView showsVerticalScrollIndicator={false}>
+      <TouchableOpacity>
+        <ListItem bottomDivider>
+          <ListItem.Content>
+            <ListItem.Subtitle>Account info update </ListItem.Subtitle>
+          </ListItem.Content>
+        </ListItem>
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <ListItem bottomDivider>
+          <ListItem.Content>
+            <ListItem.Subtitle>Update KYC </ListItem.Subtitle>
+          </ListItem.Content>
+        </ListItem>
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <ListItem bottomDivider>
+          <ListItem.Content>
+            <ListItem.Subtitle>Change Password </ListItem.Subtitle>
+          </ListItem.Content>
+        </ListItem>
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <ListItem bottomDivider>
+          <ListItem.Content>
+            <ListItem.Subtitle>Update Email Address </ListItem.Subtitle>
+          </ListItem.Content>
+        </ListItem>
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <ListItem bottomDivider>
+          <ListItem.Content>
+            <ListItem.Subtitle>Notification Settings </ListItem.Subtitle>
+          </ListItem.Content>
+        </ListItem>
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <ListItem bottomDivider>
+          <ListItem.Content>
+            <ListItem.Subtitle>View BVN </ListItem.Subtitle>
+          </ListItem.Content>
+        </ListItem>
+      </TouchableOpacity>
+    </ScrollView>
+  );
+};
+
+export default AccountSettings;
