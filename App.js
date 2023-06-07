6d3a1889db41eeaa89d93a07f76c1b3db37e9324
Aremu Oluwaseyi Festus <oluwaremu1407@gmail.com>
2023-06-07 17:20:37 +0100

    Update navigation with new screen added

--- App.js
+++ App.js
@@ -1,8 +1,11 @@
 import { StatusBar } from "expo-status-bar";
-import { LoginScreen } from "./components/Login";
+import { LoginScreen } from "./components/User/Login";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import { SignUpScreen } from "./components/Signup";
+import { SignUpScreen } from "./components/User/Signup";
+import { DashboardScreen } from "./components/Dashboard";
+import { KycScreen } from "./components/User/KYC";
+import { ProfileScreen } from "./components/User/Profile/Profile";
 const Stack = createStackNavigator();
 
 export default function App() {
@@ -15,6 +18,9 @@ export default function App() {
       >
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="Signup" component={SignUpScreen} />
+        <Stack.Screen name="Dashboard" component={DashboardScreen} />
+        <Stack.Screen name="Kyc" component={KycScreen} />
+        <Stack.Screen name="Profile" component={ProfileScreen} />
       </Stack.Navigator>
       <StatusBar style="auto" />
     </NavigationContainer>
