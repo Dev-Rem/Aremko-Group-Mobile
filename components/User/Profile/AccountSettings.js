de6063f58f79269b2a8c6e61824f9bb13a6c99e0
Aremu Oluwaseyi Festus <oluwaremu1407@gmail.com>
2023-06-08 16:27:05 +0100

    Add overlays for account settings options

--- components/User/Profile/AccountSettings.js
+++ components/User/Profile/AccountSettings.js
@@ -1,64 +1,138 @@
 import React, { useState } from "react";
-import { ScrollView, StyleSheet, TouchableOpacity, Text } from "react-native";
+import {
+  ScrollView,
+  StyleSheet,
+  TouchableOpacity,
+  Text,
+  Modal,
+  View,
+  Dimensions,
+} from "react-native";
 import { ListItem } from "@rneui/themed";
 import { Icon } from "@rneui/themed";
+import UpdateAccountInfoOverlay from "./UpdateAccountInfo";
+import UpdateKycOverlay from "./UpdateKyc";
+import ChangePasswordOverlay from "./ChangePassword";
+import UpdateEmailAddressOverlay from "./UpdateEmailAddress";
+const { width, height } = Dimensions.get("window");
 
 const AccountSettingsOverlay = () => {
+  const [showOverlay, setShowOverlay] = React.useState(false);
+  const [selectedItem, setSelectedItem] = React.useState(null);
+
+  const openOverlay = (item) => {
+    setSelectedItem(item);
+    setShowOverlay(true);
+  };
+
+  const closeOverlay = () => {
+    setShowOverlay(false);
+  };
   return (
     <ScrollView showsVerticalScrollIndicator={false}>
-      <Text style={style.headText}>Account Settings</Text>
-      <TouchableOpacity>
-        <ListItem bottomDivider>
-          <ListItem.Content>
-            <ListItem.Subtitle>Account info update </ListItem.Subtitle>
-          </ListItem.Content>
-        </ListItem>
-      </TouchableOpacity>
-      <TouchableOpacity>
-        <ListItem bottomDivider>
-          <ListItem.Content>
-            <ListItem.Subtitle>Update KYC </ListItem.Subtitle>
-          </ListItem.Content>
-        </ListItem>
-      </TouchableOpacity>
-      <TouchableOpacity>
-        <ListItem bottomDivider>
-          <ListItem.Content>
-            <ListItem.Subtitle>Change Password </ListItem.Subtitle>
-          </ListItem.Content>
-        </ListItem>
-      </TouchableOpacity>
-      <TouchableOpacity>
-        <ListItem bottomDivider>
-          <ListItem.Content>
-            <ListItem.Subtitle>Update Email Address </ListItem.Subtitle>
-          </ListItem.Content>
-        </ListItem>
-      </TouchableOpacity>
-      <TouchableOpacity>
-        <ListItem bottomDivider>
-          <ListItem.Content>
-            <ListItem.Subtitle>Notification Settings </ListItem.Subtitle>
-          </ListItem.Content>
-        </ListItem>
-      </TouchableOpacity>
-      <TouchableOpacity>
-        <ListItem bottomDivider>
-          <ListItem.Content>
-            <ListItem.Subtitle>View BVN </ListItem.Subtitle>
-          </ListItem.Content>
-        </ListItem>
-      </TouchableOpacity>
+      <View style={styles.container}>
+        <Text style={styles.headText}>Account Settings</Text>
+        <TouchableOpacity onPress={() => openOverlay("accountInfoUpdate")}>
+          <ListItem bottomDivider>
+            <ListItem.Content>
+              <ListItem.Subtitle>Account info update </ListItem.Subtitle>
+            </ListItem.Content>
+          </ListItem>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => openOverlay("updateKyc")}>
+          <ListItem bottomDivider>
+            <ListItem.Content>
+              <ListItem.Subtitle>Update KYC </ListItem.Subtitle>
+            </ListItem.Content>
+          </ListItem>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => openOverlay("changePassword")}>
+          <ListItem bottomDivider>
+            <ListItem.Content>
+              <ListItem.Subtitle>Change Password </ListItem.Subtitle>
+            </ListItem.Content>
+          </ListItem>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => openOverlay("updateEmail")}>
+          <ListItem bottomDivider>
+            <ListItem.Content>
+              <ListItem.Subtitle>Update Email Address </ListItem.Subtitle>
+            </ListItem.Content>
+          </ListItem>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => openOverlay("updateKyc")}>
+          <ListItem bottomDivider>
+            <ListItem.Content>
+              <ListItem.Subtitle>Notification Settings </ListItem.Subtitle>
+            </ListItem.Content>
+          </ListItem>
+        </TouchableOpacity>
+      </View>
+
+      <Modal visible={showOverlay} animationType="slide" transparent={true}>
+        <View style={styles.overlay}>
+          <View style={styles.overlayContent}>
+            <TouchableOpacity style={styles.closeButton} onPress={closeOverlay}>
+              <Icon name="close" type="material" color="black" size={30} />
+            </TouchableOpacity>
+            {selectedItem === "accountInfoUpdate" ? (
+              <UpdateAccountInfoOverlay />
+            ) : (
+              <></>
+            )}
+            {selectedItem === "updateKyc" ? <UpdateKycOverlay /> : <></>}
+            {selectedItem === "updateEmail" ? (
+              <UpdateEmailAddressOverlay />
+            ) : (
+              <></>
+            )}
+
+            {selectedItem === "changePassword" ? (
+              <ChangePasswordOverlay />
+            ) : (
+              <></>
+            )}
+          </View>
+        </View>
+      </Modal>
     </ScrollView>
   );
 };
 
-const style = StyleSheet.create({
+const styles = StyleSheet.create({
   headText: {
     backgroundColor: "white",
     paddingLeft: 12,
     paddingTop: 10,
     fontSize: 20,
+    fontWeight: "bold",
+  },
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+    height: height * 0.7,
+    width: width * 1.0,
+    justifyContent: "flex-start",
+  },
+
+  overlay: {
+    flex: 1,
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+    justifyContent: "flex-end",
+    alignItems: "center",
+  },
+  overlayContent: {
+    backgroundColor: "#d6d6d6",
+    width: "100%",
+    borderRadius: 8,
+    paddingTop: 40,
+    flexDirection: "column",
+  },
+  closeButton: {
+    position: "absolute",
+    top: 10,
+    right: 10,
+    zIndex: 1,
   },
 });
 export default AccountSettingsOverlay;
---
+++ components/User/Profile/UpdateEmailAddress.js
@@ -0,0 +1,110 @@
+import React, { useState } from "react";
+import {
+  ScrollView,
+  StyleSheet,
+  TouchableOpacity,
+  TextInput,
+  Text,
+  View,
+  Dimensions,
+  KeyboardAvoidingView,
+} from "react-native";
+import { Button } from "@rneui/themed";
+
+const { width, height } = Dimensions.get("window");
+
+const UpdateEmailAddressOverlay = () => {
+  return (
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : undefined}
+    >
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View style={styles.container}>
+          <Text style={styles.headText}>Update Email</Text>
+
+          <View style={styles.formContainer}>
+            <Text style={styles.label}>New Email Address</Text>
+            <TextInput
+              style={styles.input}
+              placeholder="e.g. example@email.com"
+            />
+            <Text style={styles.label}>Password</Text>
+            <TextInput
+              style={styles.input}
+              secureTextEntry
+              placeholder="your login password"
+            />
+          </View>
+          <Button
+            style={styles.button}
+            title="VERIFY EMAIL"
+            titleStyle={{ fontWeight: "bold", color: "white" }}
+            type="clear"
+          />
+        </View>
+      </ScrollView>
+    </KeyboardAvoidingView>
+  );
+};
+
+const styles = StyleSheet.create({
+  headText: {
+    backgroundColor: "white",
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+    height: height * 0.7,
+    width: width * 1.0,
+    justifyContent: "flex-start",
+    padding: 30,
+  },
+  formContainer: {
+    justifyContent: "center",
+  },
+  label: {
+    fontSize: 12,
+    fontWeight: "bold",
+    marginBottom: 10,
+    marginTop: 20,
+  },
+  input: {
+    padding: 10,
+    borderRadius: 5,
+    fontSize: 15,
+    width: "100%",
+    backgroundColor: "white",
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.4,
+    shadowRadius: 4,
+  },
+  button: {
+    marginTop: 20,
+    backgroundColor: "#922268",
+    borderColor: "white",
+    borderRadius: 5,
+  },
+  overlay: {
+    flex: 1,
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+    justifyContent: "flex-end",
+    alignItems: "center",
+  },
+  overlayContent: {
+    backgroundColor: "#d6d6d6",
+    width: "100%",
+    borderRadius: 8,
+    paddingTop: 40,
+    flexDirection: "column",
+  },
+  closeButton: {
+    position: "absolute",
+    top: 10,
+    right: 10,
+    zIndex: 1,
+  },
+});
+export default UpdateEmailAddressOverlay;
